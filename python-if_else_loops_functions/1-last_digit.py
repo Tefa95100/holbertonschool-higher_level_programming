0b02addf90fdf9ed538ccb4b14369d14341b260d
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-15 18:23:29 +0100

    Remove while

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number)
-while last_digit > 9:
-    last_digit %= 10
+last_digit %= 10
 if last_digit > 5:
     if number < 0:
         print("Last digit of {} is -{} and is and is less than 6 and not 0"
