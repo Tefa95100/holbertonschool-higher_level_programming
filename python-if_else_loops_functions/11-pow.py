21efec083a765bc8a6e8898dadc61e2fe7f9ea08
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-16 17:38:53 +0100

    Change by arythmetic operator for pow

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = 1
-    index = 0
     if b == 0:
         return result
-    if b < 0:
-        while index < abs(b):
-            result /= a
-            index += 1
     else:
-        while index < b:
-            result *= a
-            index += 1
+        result = a ** b
     return result
