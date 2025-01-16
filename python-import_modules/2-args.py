9873a129b47ba9e5cefc1432c942cd41f1e8bafe
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-16 15:02:19 +0100

    Correction for case have one argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,10 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
 
-    if len(argv) - 1 > 0:
-        separator_arg = ":"
+    if len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
+    elif len(argv) - 1 > 1:
+        print("{} arguments:".format(len(argv) - 1))
     else:
-        separator_arg = "."
-    print("{} argument{}".format(len(argv) - 1, separator_arg))
+        print("{} arguments.".format(len(argv) - 1,))
     for arg_number in range(1, len(argv)):
         print("{}: {}".format(arg_number, argv[arg_number]))
