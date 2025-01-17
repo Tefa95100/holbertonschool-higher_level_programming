270246be73f161a98b2f683332965741fb07e576
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-17 15:00:18 +0100

    Add verification in case or n is negative

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,8 @@
 
 def remove_char_at(str, n):
     str_cpy = str + ''
-
-    if n < len(str_cpy):
+    if n < 0:
+        return str_cpy
+    elif n < len(str_cpy):
         str_cpy = str_cpy.replace(str_cpy[n], '')
     return str_cpy
