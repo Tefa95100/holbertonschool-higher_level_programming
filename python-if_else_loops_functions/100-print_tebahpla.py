e3bd9f67f86438d88d8dc803d63ba91544f0e1c6
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-17 17:21:37 +0100

    Pycodestyle compliance

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for index in range(0, 26):
-    print("{}".format(chr(122 - index) if index % 2 == 0 else chr(90 - index)), end='')
+    print("{}".format(chr(122 - index) if index % 2 == 0
+                      else chr(90 - index)), end='')
