8dace4dad0c49e5809cab7a8dcc72a4f3a60b693
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-21 15:29:07 +0100

    Add missing specificateur for string format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for index_row in matrix:
         for element in index_row:
             if isinstance(element, int):
-                print("{}".format(element), end=" ")
+                print("{:d}".format(element), end=" ")
         print()
