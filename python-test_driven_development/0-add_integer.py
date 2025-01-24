3a7b4aa5561e56dc4527cf8dce3fa90368795883
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-24 18:33:12 +0100

    Add commentary and add test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+0-add_integer.py
+
+This modul contain a function for add 2 numbers
+"""
 def add_integer(a, b=98):
+    """
+    Add two numbers and return result
+
+    Parameters:
+    a: the first number
+    b: the second number
+
+    Return:
+    return the sum of a and b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
