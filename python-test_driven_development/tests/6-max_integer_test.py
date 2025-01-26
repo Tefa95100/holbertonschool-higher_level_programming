abc17a5927f1828b77905441a0d3487c3472a139
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-26 22:22:58 +0100

    Correction pycodestyle

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,20 +7,20 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-	def test_list_in_order(self):
-		self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_list_in_order(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-	def test_list_not_order(self):
-		self.assertEqual(max_integer([1, 4, 3, 2]), 4)
+    def test_list_not_order(self):
+        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
 
-	def test_one_element(self):
-		self.assertEqual(max_integer([4]), 4)
+    def test_one_element(self):
+        self.assertEqual(max_integer([4]), 4)
 
-	def test_empty_list(self):
-		self.assertEqual(max_integer([]), None)
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
 
-	def test_no_argument(self):
-		self.assertEqual(max_integer(), None)
+    def test_no_argument(self):
+        self.assertEqual(max_integer(), None)
 
-	def test_a_string(self):
-		self.assertEqual(max_integer("Hello"), "o")
+    def test_a_string(self):
+        self.assertEqual(max_integer("Hello"), "o")
