441a7137dcf6aba28fd6710b0431670314903c07
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-28 15:51:33 +0100

    First send for correction

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+1-square.py
+
+This module contain one class Square
+"""
+
+
+class Square:
+    """
+    Class Square contain a function to init attribute of object
+
+    Args:
+    size (int): the size of square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
