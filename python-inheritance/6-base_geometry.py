256fcaa306581d1c2b7c45681e28a992cda96bf9
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-03 20:32:53 +0100

    Add exercise for correction

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+Contain a empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Contain a function who raise an exception
+    """
+    def __init__(self):
+        """
+        Empty init
+        """
+        pass
+
+    def area(self):
+        """Raise an exception
+
+        Raises:
+            Exception: Raise when the function area is call
+        """
+        raise Exception('area() is not implemented')
