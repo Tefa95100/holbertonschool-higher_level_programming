a513b399fecf824d8aea3f434286c2afef1b84dd
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-04 02:10:33 +0100

    Correction pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,21 +6,21 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-	"""
-	Class Rectangle inherit of BaseGeometry
+    """
+    Class Rectangle inherit of BaseGeometry
 
-	Args:
-		BaseGeometry (class): class herited
-	"""
-	def __init__(self, width, height):
-		"""
-		Initialize the parameters when you create the object
+    Args:
+        BaseGeometry (class): class herited
+    """
+    def __init__(self, width, height):
+        """
+        Initialize the parameters when you create the object
 
-		Args:
-			width (int): the width of the rectangle
-			height (int): the height of the rectangle
-		"""
-		if self.integer_validator(width, width):
-			self.__width = width
-		if not self.integer_validator(height, height):
-			self.__height = height
+        Args:
+            width (int): the width of the rectangle
+            height (int): the height of the rectangle
+        """
+        if self.integer_validator(width, width):
+            self.__width = width
+        if not self.integer_validator(height, height):
+            self.__height = height
