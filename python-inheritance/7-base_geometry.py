dab370129cc80327b501e71736a3412df8371140
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-04 10:58:11 +0100

    Correction for return True if the value is ok

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -37,5 +37,7 @@ class BaseGeometry:
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        else:
+            return True
