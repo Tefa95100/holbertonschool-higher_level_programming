7db4354addf644449888570061d763dd5a2d357c
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-04 15:42:19 +0100

    Add exercise for correction

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Contain a class Square
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """
+        initialize the square
+
+        Args:
+            size (int): call super for initialize the square
+        """
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Methods to print the rectangle
+
+        Returns:
+            str: return the method to print the rectangle
+        """
+        return super.__str__()
+
+    def area(self):
+        """
+        call super for the area function
+
+        Returns:
+            int: the area of square
+        """
+        return super().area()
