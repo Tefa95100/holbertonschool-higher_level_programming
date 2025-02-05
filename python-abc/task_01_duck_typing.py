b3ad3d4f065ab2d5d1cf0076bf8c0d9a815027a7
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-05 21:41:52 +0100

    Add exercise for first correction

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+from math import pi
+"""
+Contains class and function for use methods
+"""
+
+
+class Shape(ABC):
+    """
+    Contain abstract method
+    """
+    @abstractmethod
+    def area():
+        """
+        abstract method for area
+        """
+        pass
+
+    @abstractmethod
+    def perimeter():
+        """
+        abstract method for perimeter
+        """
+        pass
+
+
+class Circle(Shape):
+    """
+    Class for create a circle with method for recuperate info
+    """
+    def __init__(self, radius):
+        """
+        Initialize the circle
+
+        Args:
+            radius (int): the radius of circle
+        """
+        self.radius = radius
+
+    def area(self):
+        """
+        Calculate the area of circle
+
+        Returns:
+            int: return the area of circle
+        """
+        return (self.radius ** 2) * pi
+
+    def perimeter(self):
+        """
+        Calculate the perimeter of circle
+
+        Returns:
+            int: return the perimer of circle
+        """
+        return (self.radius * 2) * pi
+
+
+class Rectangle(Shape):
+    """
+    Class for create a rectangle with method for recuperate info
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """
+        Calculate the area of rectangle
+
+        Returns:
+            int: return the area of rectangle
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+        Calculate the perimeter of rectangle
+
+        Returns:
+            int: return the perimer of rectangle
+        """
+        return (self.width + self.height) * 2
+
+
+def shape_info(object):
+    """
+    Print the are and the perimeter of the object receive
+
+    Args:
+        object (object): object for recuperate the area and perimeter
+    """
+    print(f"Area: {object.area()}")
+    print(f"Perimeter: {object.perimeter()}")
