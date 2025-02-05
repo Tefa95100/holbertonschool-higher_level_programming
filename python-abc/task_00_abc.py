fe9f72d9df9d904cb93b499a9bb23e3e35642df0
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-05 16:57:51 +0100

    Add doc

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,17 +1,45 @@
+#!/usr/bin/python3
+"""
+Contains an abstract class and 2 subclass
+"""
 from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
+    """
+    Abstract class with method for herited
+    """
     @abstractmethod
     def sound(self):
+        """
+        Abstract method for force creation in subclass
+        """
         pass
 
 
 class Dog(Animal):
+    """
+    Class with one method
+    """
     def sound(self):
+        """
+        methods for give the sound of animal
+
+        Returns:
+            string: the sound of a dog
+        """
         return "Bark"
 
 
 class Cat(Animal):
+    """
+    Class with one method
+    """
     def sound(self):
+        """
+        methods for give the sound of animal
+
+        Returns:
+            string: the sound of a cat
+        """
         return "Meow"
