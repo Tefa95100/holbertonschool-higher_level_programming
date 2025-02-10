943f122596da06f2cfcfff397fa078064ad3f68e
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-10 10:18:35 +0100

    Add exercise for first correction

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contain a function for read and display in stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Read and display on stdout
+
+    Args:
+        filename (str): The destination fo file you need read. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
