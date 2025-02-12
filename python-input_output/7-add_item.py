a78bbf5d0ce80c51276c855dc444a978f574126c
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-12 10:37:29 +0100

    Change place of import and add doc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+Main when your load a json and add all element of a list
+"""
+import os
+import sys
+save = __import__("5-save_to_json_file").save_to_json_file
+load = __import__("6-load_from_json_file").load_from_json_file
+
 if __name__ == "__main__":
-    import os
-    import sys
-    save = __import__("5-save_to_json_file").save_to_json_file
-    load = __import__("6-load_from_json_file").load_from_json_file
 
     filename = "add_item.json"
 
