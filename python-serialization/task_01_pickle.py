0919b72b813b24061fa47bef9dddea8df71940e3
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-13 14:32:24 +0100

    Change return for exception on None and change indentation by space for pycodestyle

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,60 +6,60 @@ import pickle
 
 
 class CustomObject:
-	"""
-	A custome object for learn serialization
-	"""
-	def __init__(self, name, age, is_student):
-		"""
-		Initialize an instance
+    """
+    A custome object for learn serialization
+    """
+    def __init__(self, name, age, is_student):
+        """
+        Initialize an instance
 
-		Args:
-			name (str): name of student
-			age (int): age of student
-			is_student (bool): Say if is a student or others
-		"""
-		self.name = name
-		self.age = age
-		self.is_student = is_student
-	
-	def serialize(self, filename):
-		"""
-		Serialize an instance with pickle
+        Args:
+            name (str): name of student
+            age (int): age of student
+            is_student (bool): Say if is a student or others
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+    
+    def serialize(self, filename):
+        """
+        Serialize an instance with pickle
 
-		Args:
-			filename (str): path of file for save
+        Args:
+            filename (str): path of file for save
 
-		Returns:
-			exception: return exception
-		"""
-		try:
-			with open(filename, "wb") as file:
-				pickle.dump(self.__dict__, file)
-		except Exception as e:
-			return e
-	
-	def display(self):
-		"""
-		Display information of student
-		"""
-		print(f"Name: {self.name}")
-		print(f"Age: {self.age}")
-		print(f"Is Student: {self.is_student}")
+        Returns:
+            exception: return exception
+        """
+        try:
+            with open(filename, "wb") as file:
+                pickle.dump(self.__dict__, file)
+        except pickle.PickleError:
+            return None
+    
+    def display(self):
+        """
+        Display information of student
+        """
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
-	@classmethod
-	def deserialize(cls, filename):
-		"""
-		Initialize a student after load from a file
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Initialize a student after load from a file
 
-		Args:
-			filename (str): path of file
+        Args:
+            filename (str): path of file
 
-		Returns:
-			exception: return an exception if fail
-		"""
-		try:
-			with open(filename, "rb") as file:
-				dict = pickle.load(file)
-				return cls(dict["name"], dict["age"], dict["is_student"])
-		except Exception as e:
-			return e
+        Returns:
+            exception: return an exception if fail
+        """
+        try:
+            with open(filename, "rb") as file:
+                dict = pickle.load(file)
+                return cls(dict["name"], dict["age"], dict["is_student"])
+        except pickle.UnpicklingError:
+            return None
