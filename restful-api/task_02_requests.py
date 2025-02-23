efd5c620661b0728a3c1523bdfa680f1b160ad29
Tefa95100 <damien.julien.95100@gmail.com>
2025-02-23 22:59:42 +0100

    Add message for success of failed, add line for pycodestyle and add parameters in open file

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -23,10 +23,15 @@ def fetch_and_save_posts():
 
     if response.status_code == 200:
         data = response.json()
-        dict_data = [{"id": word["id"], "title": word["title"], "body": word["body"]} for word in data]
+        dict_data = [{"id": word["id"], "title": word["title"],
+                      "body": word["body"]} for word in data]
 
-    with open(file_csv, "w", encoding="utf-8") as csvfile:
-        field_name = ["id", "title", "body"]
-        writer = csv.DictWriter(csvfile, fieldnames=field_name)
-        writer.writeheader()
-        writer.writerows(dict_data)
+        with open(file_csv, "w", newline="", encoding="utf-8") as csvfile:
+            field_name = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=field_name)
+            writer.writeheader()
+            writer.writerows(dict_data)
+
+        print("Success saved to posts.csv")
+    else:
+        print("Failed to retrieve posts")
