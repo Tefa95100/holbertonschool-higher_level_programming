5b7136ab9e6a5b3ffa8ea16f2e96ba3a50b12889
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-07 09:08:39 +0100

    Correction where I attribute privilege and add flush

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT PRIVILEGE ON hbtn_0d_2 to 'user_0d_2'@'localhost';
+GRANT PRIVILEGE ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGE;
