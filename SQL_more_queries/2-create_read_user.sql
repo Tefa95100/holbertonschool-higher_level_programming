a0a57550703093fb0b9be87a035d5ae4bc0f734b
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-07 09:12:38 +0100

    Correction for word privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT PRIVILEGE ON hbtn_0d_2.*
 TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGE;
+FLUSH PRIVILEGES;
