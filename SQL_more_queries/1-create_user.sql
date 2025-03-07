732aad93ecbbc68f47c382ae7e57059d52dc45ce
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-07 09:06:16 +0100

    Add privilege after read correctly the exercise

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- Creates the MySQL server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGE ON *.* TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
+FLUSH PRIVILEGE;
