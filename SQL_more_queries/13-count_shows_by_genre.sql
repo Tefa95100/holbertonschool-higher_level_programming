3157a8b702f4715dd82613f2df63874cc740c48c
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-08 14:33:16 +0100

    Add an as for change name of column

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,10 @@
 -- Lsts all genres from hbtn_0d_tvshows
 -- and displays the number of shows 
-SELECT tv_genres.name, count(tv_show_genres.genre_id)
+SELECT tv_genres.name AS genre,
+	count(tv_show_genres.genre_id)
 AS number_of_shows
 FROM tv_show_genres
-INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id=tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
