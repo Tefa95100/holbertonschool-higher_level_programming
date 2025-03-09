b40b430c87ff076b31e94cd0e69b085dd219e2dc
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-10 00:05:00 +0100

    Pycodestyle and change shebang

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 search a state in database
 """
@@ -22,7 +22,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".
+                   format(sys.argv[4]))
 
     for row in cursor.fetchall():
         print(row)
