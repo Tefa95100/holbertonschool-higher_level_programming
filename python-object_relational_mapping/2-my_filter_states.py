c1140bd810a9e0474edec11177524bc8866cb723
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-09 23:59:36 +0100

    Add exercise

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+search a state in database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Search a states
+
+    Keyword arguments:
+    argument -- argument for username password and name of database
+    and states search
+    Return: return the response of script
+    """
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database, port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
