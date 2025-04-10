500302e23d08ff818248d03351ebb0d5f57d4a58
Tefa95100 <damien.julien.95100@gmail.com>
2025-04-10 10:59:13 +0200

    Change if in route add user

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -34,9 +34,17 @@ def get_username(username):
 @app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
-    if not data or "username" not in data or "name" not in data:
-        if "age" not in data or "city" not in data:
-            return jsonify({"Error: field missing"}), 400
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    if "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+    if "name" not in data:
+        return jsonify({"error": "Name is required"}), 400
+    if "age" not in data:
+        return jsonify({"error": "Age is required"}), 400
+    if "city" not in data:
+        return jsonify({"error": "City is required"}), 400
 
     username = data["username"]
     if username in users:
