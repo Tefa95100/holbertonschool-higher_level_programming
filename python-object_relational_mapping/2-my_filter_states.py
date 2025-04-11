14e1f32ba6c0509806fbd900ea29e93bb4659eeb
Tefa95100 <damien.julien.95100@gmail.com>
2025-04-11 10:57:56 +0200

    Correction pycodestyle

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,8 +22,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".
-                   format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' "
+                   "ORDER BY id ASC".format(sys.argv[4]))
 
     for row in cursor.fetchall():
         print(row)
