52d40bb92be25c70f1825e19a8f71c6b225ce580
Tefa95100 <damien.julien.95100@gmail.com>
2025-04-11 11:18:27 +0200

    Add exercise 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script for search city by state
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Search city by states
+
+    Keyword arguments:
+    argument -- argument for username password and name of database
+    and states search
+    Return: return the response of script
+    """
+
+    username, password, database, state = sys.argv[1:5]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database, port=3306)
+
+    cursor = db.cursor()
+
+    query = """SELECT cities.name FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC;"""
+    cursor.execute(query, (state,))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
