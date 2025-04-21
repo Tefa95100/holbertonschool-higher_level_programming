1238e2f6e947238a6bba6909d5fb51dfe6946461
Tefa95100 <damien.julien.95100@gmail.com>
2025-04-21 11:02:56 +0200

    Correction of exercise 19 with add shebang, pycodestyle and add new line EOF and change response of 25

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+"""
+Copy a list
+"""
+
+
 def copy_list(a_list):
 
-	"""Returns a copy of a list."""
-	return a_list.copy()
+    """
+    Returns a copy of a list.
+    """
+    return a_list.copy()
